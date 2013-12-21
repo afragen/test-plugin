ab13dae3a3d7204de16c8054c15922aeb86a84cd
Andy Fragen <andy@thefragens.com>
2013-12-20 20:10:53 -0800

    bump

--- test-plugin.php
+++ test-plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Test Plugin
 Plugin URI: https://github.com/afragen/test-plugin/
 GitHub Plugin URI: afragen/test-plugin/
 Description: This plugin is used for testing functionality of Github updating of plugins.
-Version: 0.7.1
+Version: 0.7.2
 Author: Andy Fragen
 License: GNU General Public License v2
 License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
