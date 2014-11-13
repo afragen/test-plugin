2bee8fefe469afb33dd1cfa414e569e9ae283f92
Andy Fragen <andy@thefragens.com>
2014-11-13 15:23:14 -0800

    testing requirements

--- test-plugin.php
+++ test-plugin.php
@@ -2,11 +2,14 @@
 /*
 Plugin Name: Test Plugin
 Plugin URI: https://github.com/afragen/test-plugin/
-GitHub Plugin URI: afragen/test-plugin/
 Description: This plugin is used for testing functionality of Github updating of plugins.
-Version: 0.7.2
+Version: 0.7.3
 Author: Andy Fragen
 License: GNU General Public License v2
 License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/afragen/test-plugin/
+GitHub Branch: master
+Requires WP: 4.0
+Requires PHP: 5.4
 */
 
