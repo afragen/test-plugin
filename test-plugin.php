9b586a1eac3f4588c36400362de52709ee7a2656
Andy Fragen <andy@thefragens.com>
2019-11-06 14:18:58 -0800

    high PHP to fail

--- test-plugin.php
+++ test-plugin.php
@@ -3,14 +3,11 @@
  * Plugin Name: Test Plugin
  * Plugin URI: https://github.com/afragen/test-plugin/
  * Description: This plugin is used for testing functionality of Github updating of plugins.
- * Version: 0.8.0
+ * Version: 0.1.0
  * Author: Andy Fragen
  * License: GNU General Public License v2
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * GitHub Plugin URI: https://github.com/afragen/test-plugin/
- * GitHub Branch: master
- * GitHub Languages: afragen/github-updater-translations
- * Requires WP: 4.0
- * Requires PHP: 5.4
+ * Requires at least: 4.0
+ * Requires PHP: 8.0
  */
-
